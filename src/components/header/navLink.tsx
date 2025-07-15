e55ff60123e3967d6d93e4a7aa2f485ecd07db44
LauraBLei <laura.blei@outlook.com>
2025-07-15 12:20:17 +0200

    feat: added correct styling to sidebar and desktop

--- src/components/header/navLink.tsx
+++ src/components/header/navLink.tsx
@@ -1,16 +1,17 @@
+import { useLocale } from "next-intl";
 import Link from "next/link";
 
 interface Props {
   href: string;
   text: string;
+  onClick?: () => void;
 }
 
-export const NavLink = ({ href, text }: Props) => {
+export const NavLink = ({ href, text, onClick }: Props) => {
+  const locale = useLocale();
+  const linkHref = locale === "da" ? href : `/en${href}`;
   return (
-    <Link
-      href={href}
-      className="lg:text-xl hover:text-brand-blue transition-colors"
-    >
+    <Link href={linkHref} onClick={onClick} className="block navText">
       {text}
     </Link>
   );
