f87e6aa2c8077b8ba0fb0d740dd0633b61f4599b
LauraBLei <laura.blei@outlook.com>
2025-07-15 14:00:15 +0200

    feat: added ValueProps Component

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,10 +1,13 @@
 import { useTranslations } from "next-intl";
+import { ValueProps } from "../valueProps";
 
 export const HomePage = () => {
   const t = useTranslations("HomePage");
   return (
     <div>
       <h1>{t("Title")}</h1>
+
+      <ValueProps />
     </div>
   );
 };
