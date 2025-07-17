7316e0d9a3668a8e51a3d053afb96e39e04dc9f3
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-07-17 14:52:34 +0200

    Feat: Adds card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,1 +1,26 @@
 "use client";
+
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+import { FloorCardProps } from "@/types/interface";
+import Image from "next/image";
+
+export const FloorCard = ({ service }: FloorCardProps) => {
+  const t = useTranslations("FloorPage.services");
+  return (
+    <Link href={"/tjenester"}>
+      <div className="card">
+        <Image
+          src={service.image}
+          alt={service.title}
+          width={400}
+          height={300}
+          style={{ objectFit: "cover" }}
+        />
+        <div>
+          <p>{t(service.title)}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
