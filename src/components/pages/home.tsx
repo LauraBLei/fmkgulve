c752f1dc981f548ec48f55f955ca2a109180213d
LauraBLei <laura.blei@outlook.com>
2025-07-23 10:35:40 +0200

    added hero section

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,13 +1,20 @@
 import { useTranslations } from "next-intl";
 import { ValueProps } from "../valueProps";
+import { HeroSection } from "../home/hero";
 
 export const HomePage = () => {
   const t = useTranslations("HomePage");
   return (
     <div>
-      <h1>{t("Title")}</h1>
-
-      <ValueProps />
+      <h1 className="sr-only">{t("Title")}</h1>
+      <section id="hero">
+        <HeroSection />
+      </section>
+      <section className="value">
+        <ValueProps />
+      </section>
+      <section id="services"></section>
+      <section id="contact"></section>
     </div>
   );
 };
