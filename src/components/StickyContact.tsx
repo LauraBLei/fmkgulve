27f24729bffd3961b1861f8f55af610d5351f92f
LauraBLei <laura.blei@outlook.com>
2025-07-24 11:36:49 +0200

    added: Call now calls number

--- src/components/StickyContact.tsx
+++ src/components/StickyContact.tsx
@@ -8,7 +8,9 @@ export const StickyContact = () => {
     <div className="fixed z-50 bottom-0 lg:hidden left-0 bg-white text-brand-blue w-full flex justify-between items-center p-3 border-1 border-brand-bluer h-21 font-semibold shadow-[0_-6px_10px_-2px_rgba(0,0,0,0.1)]">
       <div className="flex-1 flex flex-col items-center gap-1">
         <Phone size={20} />
-        <p>{t("phone")}</p>
+        <a href="tel:+4530131631" className="hover:underline">
+          {t("phone")}
+        </a>
       </div>
       <span className="border-1 border-brand-blue h-full mx-4" />
       <Link
